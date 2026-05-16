efc23cdef2188b4ab5d16838ac6f27a77855489e
lamber0421 <a0983659543@gmail.com>
2026-05-17 01:54:34 +0800

    Add files via upload

---
+++ 記帳練習.py
@@ -0,0 +1,20 @@
+#x= 收入
+#y= 支出
+#z= 储蓄
+Z = 0 
+print("你目前存款為:", Z)
+while True:
+    try:
+        x= int(input("請輸入收入:"))
+        break
+    except ValueError:
+        print("請輸入數字")
+while True:
+    try:
+        y= int(input("請輸入支出:"))
+        break
+    except ValueError:
+        print("請輸入數字")
+Z = Z + x - y
+print("你目前存款為:", Z)
+input("按Enter鍵結束")  
